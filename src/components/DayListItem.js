fa6ff84a2986c490847e3dfc1b57f07d9af002f4
Danek <danek.fill@outlook.com>
2022-08-10 08:43:43 -0400

    Big commit, realized no commits yesterday. Side menu bar and buttons built.

---
+++ src/components/DayListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props){
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
+
+  return (
+    <li 
+    className = {dayClass}
+    onClick={ () => props.setDay(props.name)}
+    >
+      <h2 className='text--regular'>{props.name}</h2>
+      {!props.spots && <h3 className='text--light'>no spots remaining</h3>}
+      {props.spots === 1 && <h3 className='text--light'>{props.spots} spot remaining</h3>}
+      {props.spots > 1 && <h3 className='text--light'>{props.spots} spots remaining</h3>}
+      
+    </li>
+  );
+}
